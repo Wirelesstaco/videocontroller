cbf9b1aefc7d58a4dd57a9a9786de7dae2a78ee3
Joe p <wirelesstaco@gmail.com>
2020-08-13 15:28:46 -0700

    Initial commit- Sending inputs to and from Touch

---
+++ socket-controller/index.js
@@ -0,0 +1,36 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+// What html file ot look at 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+//Server port
+http.listen(8080, () => {
+    console.log('listening on *:3300');
+  });
+
+// Listen to connection for any incoming events.
+io.on('connection', (socket) => {
+    console.log('a user econnected');
+
+    // Disconnect event 
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    })
+    // Recive chat message
+    socket.on('chat message',(msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg); // send msg to everyone including sender
+        io.emit('xpos', msg); 
+    });
+
+    socket.on('xpos',(msg) => {
+      console.log('message: ' + msg);
+      io.emit('xpos', msg); // send msg to everyone including sender
+  });
+});
+
+
